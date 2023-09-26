c5add165e22539e91a520587f4bbcf10a3dc4821
Tinashe Matanda <tinashelorenzi@protonmail.com>
2023-09-26 21:00:34 +0000

    Minor fixes

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,7 +1,15 @@
-function taskFirst(){
-    const name = "Jeff";
-}
-
-function taskNext() {
-    var cost = "Remains unknown";
-}
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
