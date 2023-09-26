7d64217fca0e67669c79833e080e2e2051cf672e
Tinashe Matanda <tinashelorenzi@protonmail.com>
2023-09-26 21:17:57 +0000

    Edits

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+export default function taskBlock(trueOrFalse) 
+{
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      // eslint-disable
+      const task = true;
+      // eslint-disable
+      const task2 = false;
+    }
+  
+    return [task, task2];
+}
